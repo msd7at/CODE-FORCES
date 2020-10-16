821f77af3321f7958d8dd3d3327abe074ef6d052
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-10-16 06:33:50 +0530

    Create #661 (Div. 3) D. Binary String To Subsequences.py

---
+++ #661 (Div. 3) D. Binary String To Subsequences.py
@@ -0,0 +1,35 @@
+t=int(input())
+for q in range(t):
+    n=int(input())
+    s=input()
+    az=[]
+    ao=[]
+    cc=1
+    ans=[]
+    j=0
+    for i in s:
+        if i=="0":
+            l=len(ao)
+            if l==0:
+                az.append(cc)
+                ans.append(cc)
+                cc+=1
+            else:
+                p=ao.pop()
+                az.append(p)
+                ans.append(p)
+
+        else:
+            l=len(az)
+            if l==0:
+                ao.append(cc)
+                ans.append(cc)
+                cc+=1
+            else:
+                p=az.pop()
+                ao.append(p)
+                ans.append(p)
+        
+    print(cc-1)
+    print(*ans,end=" ")
+    print()
